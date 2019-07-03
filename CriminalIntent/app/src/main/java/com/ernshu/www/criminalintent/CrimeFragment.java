c40a2a16eb561242788c121ed7976dbf9926e219
erniecho <erniecho@gmail.com>
2019-07-02 22:39:59 -0400

    added partial Dialogs, calander not showing up correctly.

--- CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/CrimeFragment.java
@@ -1,8 +1,6 @@
 package com.ernshu.www.criminalintent;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
@@ -43,16 +41,15 @@ public class CrimeFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //retrieving the UUID from the fragment arguments.
         UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         //passing layout ID resource ID, create a view parent, configure the widget, connect inflate to view.
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
--- CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/DatePickerFragment.java
@@ -1,21 +1,24 @@
 package com.ernshu.www.criminalintent;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class DatePickerFragment extends DialogFragment {
-    @NonNull
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        /*single View object - a DatePicker that you will inflate and pass into setView(...)*/
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
         /* In this implemetation, you use the AlertDialog.Builder class, which provides a
         * fluent interface for constructing an AlterDialog instance. First, you pass a
         * Context into the AlertDialog.Builder constructor, which returns an instance of
         * AlertDialog.Builder.*/
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
