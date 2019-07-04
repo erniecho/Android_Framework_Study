7c28056ea0a84cae356e3dd51c851700b67113b9
erniecho <erniecho@gmail.com>
2019-07-03 22:59:23 -0400

    Complete Dialog and pass date from one fragment back.pg247

--- CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package com.ernshu.www.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,6 +15,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static android.widget.CompoundButton.*;
@@ -21,6 +24,8 @@ public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+
+    private static final int REQUEST_DATE = 0;
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -75,15 +80,23 @@ public class CrimeFragment extends Fragment {
         });
         //Button connection listener.
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        updateDate();
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*add a constant for the DatePickerFragment's tag. Then, in onCreateView(...)
                 * remove the code that disables the date button and set a View.OnClickerListener
-                * that shows a DatePicker Fragment when the date button is pressed. */
+                * that shows a DatePicker Fragment when the date button is pressed. DatePickerFragment
+                 * needs to initialize the DatePicker using the information held in the date.
+                 * However, initializing the DatePicker requires integers for the month, day, and year.
+                 * Date is more of a timestamp and cannot provide integers like this directly. */
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mCrime.getDate());
+                /*method accepts the fragment that will be the target and a request code just
+                like the one you send in startActivityForResult(...) You can retrieve them by
+                calling getTargetFragment() and getTargetRequestCode() on the fragment that has set the target. */
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(manager, DIALOG_DATE);
             }
         });
@@ -101,5 +114,24 @@ public class CrimeFragment extends Fragment {
 
 
         return v;
+    }
+    /*override onActivityResult() to retrieve the extra, set the date on the Crime,
+    and refresh the text of the date button*/
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
     }
 }
--- CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/DatePickerFragment.java
@@ -1,18 +1,59 @@
 package com.ernshu.www.criminalintent;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DatePickerFragment extends DialogFragment {
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+    public static final String EXTRA_DATE =
+            "com.ernshu.www.criminalintent.date";
+/* To get data into your DatePickerFragment, you are going to stash the date in DatePickerFragment's
+* arguments bundle, where the DatePickerFragment can access it.
+* Creating and setting fragment arguments is typically done in a newInstance(Date) method that replaces the
+* fragment constructor. In DatePickerFragment.java, add a newInstance(Date) method. */
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        /*To get the integers you need, you must create a Calander object and use the date
+        * configure the Calander. Then you can retrive the requred information from the Calander. */
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
         /*single View object - a DatePicker that you will inflate and pass into setView(...)*/
         View v = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_date, null);
+
+        /*In onCreateDialog(Bundle), get the Date from the arguments and use it and a Calendar
+        * to initialize the DatePicker. */
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
+
         /* In this implemetation, you use the AlertDialog.Builder class, which provides a
         * fluent interface for constructing an AlterDialog instance. First, you pass a
         * Context into the AlertDialog.Builder constructor, which returns an instance of
@@ -20,7 +61,34 @@ public class DatePickerFragment extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle(R.string.date_picker_title)
-                .setPositiveButton(android.R.string.ok, null)
+                .setPositiveButton(android.R.string.ok,
+                        /* setPostiveButton() with an implementation od DialogInterface.OnClickListener
+                        * that retrieves the selected date and calls sendResult() */
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int year = mDatePicker.getYear();
+                                int month = mDatePicker.getMonth();
+                                int day = mDatePicker.getDayOfMonth();
+                                Date date = new GregorianCalendar(year, month, day).getTime();
+                                sendResult(Activity.RESULT_OK, date);
+                            }
+                        })
                 .create();
+    }
+    /* sendResult method. When user presses the postive button in the dialog,
+    you want to retreve the date from the DatePicker and send the result back to CrimeFragment.
+    In on CreateDialog, replace the null parameter of setPostiveButton with an implementation
+    of DialogInterface.OnClickerListener that retrieves the selected date and calls sendResult*/
+    private void sendResult(int resultCode, Date date) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, date);
+
+        getTargetFragment()
+                .onActivityResult(getTargetRequestCode(),resultCode, intent);
     }
 }
