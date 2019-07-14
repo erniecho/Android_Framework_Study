9ab2ae4d7b28e9223160912674f52a6792ad8841
erniecho <erniecho@gmail.com>
2019-07-13 22:58:56 -0400

    working on adding tablet support.

--- CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/CrimeListActivity.java
@@ -7,4 +7,11 @@ public class CrimeListActivity extends SingleFragmentActivity {
     protected Fragment createFragment(){
     return new CrimeListFragment();
     }
+    /* FrameLayout has a fragment_container layout ID, so the code in
+    * SingleFragmentActivity.onCreate() can work as before. When the activity is created, the fragment
+    * that is returned in createFragment() will appear in the lefthand pane.*/
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_twopane;
+    }
 }
--- CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/SingleFragmentActivity.java
+++ CriminalIntent/app/src/main/java/com/ernshu/www/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.ernshu.www.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,10 +13,20 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
      */
     protected abstract Fragment createFragment();
 
+
+    /* The default implementation of SingleFragmentActivity will work the same as before, but
+    * now its subclasses can choose to override getLayoutResId() to reutrn a layout other than
+    * activity_fragment.xml. You annotate getLayoutResId() with @LayoutRes to tell Android Studio
+    * that any implementation of this method should return a valid layout resource ID.*/
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activty_fragment;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activty_fragment);
+        setContentView(getLayoutResId());
         /* Fragment Manger is created on pg 148
         If AppCompatActivity is used  then use getSupportFragmentManager()
         Using Activity to describe the public class use getFragmentManger()
